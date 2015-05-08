0f865012b15e3ed8635498f725fc5079a92110ef
TJ Kells <tjkells@gmail.com>
2015-05-08 17:10:23 -0500

    Somewhat working parser!

--- test.js
+++ test.js
@@ -1,31 +1,83 @@
 var fs = require('fs');
 var marked = require('marked');
+var pygmentize = require('pygmentize-bundled')
 
+marked.setOptions({
+  highlight: function (code, lang, callback) {
+  	pygmentize({ lang: lang, format: 'html' }, code, function (err, result) {
+      callback(err, result.toString());
+    })
+  }
+});
 
 function Presentation(slides, cfg){
-	this.slides = slides;
-	this.title = cfg.title ? cfg.title : "MY Presentation";
-	this.footer = cfg.footer ? '<div class="footer">'+cfg.footer+'</div>' : "";
-	this.stylesheet = '<link rel="stylesheet" href="'+cfg.stylesheet+'"></link>';
-    this.head = '<head><title>' + this.title + '</title>' + this.stylesheet + '</head>';
-    this.body = '<body><div id="viewer"></div>';
+	var self = this;
+	self.slides = slides;
+	self.title = cfg.title ? cfg.title : "";
+	self.footer = cfg.footer ? '<div class="footer">'+ marked(cfg.footer) +'</div>' : "";
+	self.stylesheet = '<link rel="stylesheet" href="'+cfg.theme+'"></link>';
+    self.head = '<head><title>' + self.title + '</title>' + self.stylesheet + '</head>';
+    self.body = '<body><div id="viewer"></div>' + self.footer;
 
-	this.render = function(){
-		for(var i=0; i < this.slides.length; i++){
-			this.body +=  '<slide>' + marked(this.slides[i].markdown + this.footer) + '</slide>';
-		}
-		this.body +='<div id="slide-number"></div><script type="text/javascript" src="viewer.js"></script></body>'
+    var slideRenderCount = 0;
+    self.slideRendered = function(slide){
+    	slideRenderCount += 1
+    	if(slideRenderCount == self.slides.length){
+    		self.slides.forEach(function(slide){
+    			self.body += slide.html;
+    		});
+    		self.emit('ready', self)
+    	}
+    }
+
+	self.render = function(){
+		self.slides.forEach(function(slide){
+			slide.on('rendered', self.slideRendered)
+			slide.render();	
+		});
+	}
+
+
+	self.listeners = {}
+	self.on = function(evt, callback){
+		self.listeners[evt] =callback;
+	}
+
+	self.emit = function(evt, data){
+		self.listeners[evt](data);
 	}
 }
 
 Presentation.prototype = {
-	get html(){ return '<!DOCTYPE html><html>'+ this.head + this.body + '</html>'; }
+	
+	get html(){
+		var endBody = '<div id="slide-number"></div><script type="text/javascript" src="viewer.js"></script></body>';
+		return '<!DOCTYPE html><html>'+ this.head + this.body + endBody +'</html>'; 
+	}
 }
 
 function Slide(config, markdown, number){
-	this.config = config;
-	this.markdown = markdown;
-	this.number = number;
+	var self = this;
+	self.config = config;
+	self.markdown = markdown;
+	self.number = number;
+
+
+	self.render = function(){	
+		marked(self.markdown, function(err, content){
+			self.html = '<slide>' + content + '</slide>'
+			self.emit('rendered');
+		});
+	}
+
+	self.listeners = {}
+	self.on = function(evt, callback){
+		self.listeners[evt] = callback
+	}
+
+	self.emit = function(evt, data){
+		self.listeners[evt](self)
+	}
 }
 
 
@@ -34,9 +86,8 @@ var data = fs.readFileSync("./test.md", {encoding: 'utf8'});
 var slides_md = data.split('---');
 var slides = [];
 var configKeywords = {'title': {'global': true}, 'footer': {'global': true}};
-var presentationCfg = {'stylesheet': './test.css'};
+var presentationCfg = {'theme': './test.css'};
 
-if 
 
 // Parse config data from each slide
 slides_md.forEach(function(slide, slideNo){
@@ -61,4 +112,6 @@ slides_md.forEach(function(slide, slideNo){
 
 var presentation = new Presentation(slides, presentationCfg);
 presentation.render();
-console.log(presentation.html)
+presentation.on('ready', function(pres){
+	console.log(pres.html)
+});
